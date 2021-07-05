b7d57e9ab6589404a236755759fb21b3f2bda506
wgekko <62272491+wgekko@users.noreply.github.com>
2021-07-05 19:25:39 -0300

    Add files via upload

---
+++ plantill main.cpp
@@ -0,0 +1,11 @@
+// Archivo de traducción de seudocódigo a C++
+#include <program1.h>
+/**
+*    Enunciado: 
+*/
+principal                                                       // Unidad de programa principal
+// Declaraciones: A continuación se declaran las variables del programa...
+limpiar;                                                        // Limpia la pantalla.
+//Código: (a continuación de este comentario...)
+pausa;                                                          // Pausa antes de finalizar. 
+finPrincipal                                                    // Fin de unidad de programa principal.
